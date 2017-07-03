6d2170bc4ccb205471675f048fb253c41fd19d45
KiyoKando <kiyohikokando@gmail.com>
2017-07-03 14:17:13 +0900

    second commit

--- foodtaskerapp/models.py
+++ foodtaskerapp/models.py
@@ -19,7 +19,7 @@ class Customer(models.Model):
     address = models.CharField(max_length=500, blank=True)
 
     def __str__(self):
-        return self.user.get.full_name()
+        return self.user.get_full_name()
 
 class Driver(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='driver')
@@ -28,5 +28,5 @@ class Driver(models.Model):
     address = models.CharField(max_length=500, blank=True)
 
     def __str__(self):
-        return self.user.get.full_name()
+        return self.user.get_full_name()
 #define name of the strings as equal to the "name" data of the strings.
